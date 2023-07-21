1a0a89b39cc9308429e0dd1fb078a8089cabf377
onukwufor dominic <dominicrotimi@gmail.com>
2023-07-21 19:17:30 -0400

    demo

---
+++ src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,40 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator =  "student_sequence"
+    )
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
+
+    public Student(String name, String email, LocalDate dob, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+
+}
